cbabcf3c2f3dc43f4a939937878ce2e3d4e1e57d
hachimB <boubacarhachim@gmail.com>
2025-07-07 15:57:06 +0100

    commit-2

--- h-shell.c
+++ h-shell.c
@@ -36,14 +36,20 @@ char **split(char *str) {
     char **arr = malloc(sizeof(char*));
     if (!arr) return NULL;
     int i = 0;
+    // int count = 0;
     char *token;
     char **temp;
     char *delim = " ";
 
     char *strToken = strtok(str, delim);
-    if (!strToken) return NULL;
+    if (!strToken) {
+        free(arr);
+        return NULL;
+    }
+    
+    while(strToken) {
+        if (strToken[0] == '\0') continue;
 
-    while(strToken != NULL) {
         token = strdup(strToken);
         temp = realloc(arr, (i + 2) * sizeof(char*));
         if (!temp) {
@@ -71,38 +77,66 @@ int main(int argc, char *argv[]) {
         if (chars_read == -1) {
             printf("\n");
             free(line);
-            exit(1);
+            exit(0);
         }
         else {
             int line_length = length(line) - 1;
             char *new_line = malloc((line_length + 1)* sizeof(char));
             if (!new_line) return 1;
 
+            int k = 0;
             for (int i = 0; i < line_length; i++) {
-                new_line[i] = line[i];
+                if (isprint(line[i])) {
+                    new_line[k] = line[i];
+                    k++;
+                }
             }
-            new_line[line_length] = '\0';
+            new_line[k] = '\0';
 
 
             if (new_line[0] == '\0') {
+                free(new_line);
+                free(line);
                 continue;
             }
 
             if (is_space(new_line)) {
+                free(new_line);
+                free(line);
                 continue;
             }
 
             char **arr = split(new_line);
-            if (!arr) return 1;
-            
-            if (strcmp(arr[0], "exit") == 0) {
+            if (!arr) {
                 free(new_line);
-                exit(0);
+                free(line);
+                return 1;
             }
 
             int i = 0;
             while(arr[i] != NULL) {
                 i++;
+            }
+            
+            if (strcmp(arr[0], "exit") == 0) {
+                for (int j = 0; j < i; j++) {
+                free(arr[j]);
+                }
+                free(new_line);
+                free(arr);
+                free(line);
+
+                exit(0);
+            }
+
+            if (arr[0] == NULL) {
+                for (int j = 0; j < i; j++) {
+                free(arr[j]);
+                }
+                free(new_line);
+                free(arr);
+                free(line);
+                continue;
             }
 
             printf("%d\n", i);
